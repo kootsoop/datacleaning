1aa3b5a6f5f47d1e8bfbaf1df231729be045413e
Peter Kootsookos <p.kootsookos@noster.mobi>
2016-02-22 08:06:08 -0500

    Initial commit for course project

---
+++ run_analysis.R
@@ -0,0 +1,22 @@
+#
+# Data Cleaning Project : Tidy and clean the Human Activity Recognition Dataset.
+#
+# (C) Peter J. Kootsookos 2016
+#
+# Dataset used from:
+#
+# [1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. 
+#     Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly 
+#     Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). 
+#     Vitoria-Gasteiz, Spain. Dec 2012
+#
+# From: https://www.coursera.org/learn/data-cleaning/peer/FIZtT/getting-and-cleaning-data-course-project
+# You should create one R script called run_analysis.R that does the following.
+#
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names.
+# 5. From the data set in step 4, creates a second, independent tidy data set with the 
+#    average of each variable for each activity and each subject.
+
